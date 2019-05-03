add6d3c999a3a6458d9e33fc12ec2945d35f84eb
kaya <jepsonkaya@gmail.com>
2019-05-03 08:51:56 -0700

    update logic to fit wordcounter

--- WordCounter/Controllers/WordsController.cs
+++ WordCounter/Controllers/WordsController.cs
@@ -1,5 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
-using Travel.Models;
+using WordCounter.Models;
 using System.Collections.Generic;
 
 namespace WordCounter.Controllers
--- WordCounter/Models/Words.cs
+++ WordCounter/Models/Words.cs
@@ -4,42 +4,37 @@ namespace WordCounter.Models
 {
   public class Words
   {
-    public string CityName {get; set;}
-    public int Id {get; set;}
-    public string Date {get; set;}
-    public string Journal {get; set;}
-    public string Companion {get; set;}
-    private static List<Words> _instances = new List<Words> {};
+    public string userWord {get; set;}
+    // public int Id {get; set;}
+    public string userSentence {get; set;}
+    // private static List<Words> _instances = new List<Words> {};
 
-    public Words (string cityName, string date, string journal, string companion)
+    public Words (string userWord, string userSentence)
     {
-      CityName = cityName;
-      Date = date;
-      Journal = journal;
-      Companion = companion;
-      _instances.Add(this);
-      Id = _instances.Count;
+      UserWord = userWord;
+      UserSentence = userSentence;
+      // _instances.Add(this);
+      // Id = _instances.Count;
     }
 
-
-    public int GetId()
-    {
-      return Id;
-    }
-
-    public static List<Words> GetAll()
-    {
-      return _instances;
-    }
-
-    public static void ClearAll()
-    {
-      _instances.Clear();
-    }
-
-    public static Words Find(int searchId)
-    {
-      return _instances[searchId-1];
-    }
+    // public int GetId()
+    // {
+    //   return Id;
+    // }
+    //
+    // public static List<Words> GetAll()
+    // {
+    //   return _instances;
+    // }
+    //
+    // public static void ClearAll()
+    // {
+    //   _instances.Clear();
+    // }
+    //
+    // public static Words Find(int searchId)
+    // {
+    //   return _instances[searchId-1];
+    // }
   }
 }
